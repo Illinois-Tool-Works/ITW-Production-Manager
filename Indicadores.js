bf24778b9ab1f0b9a9b9741849a7d206fc9c0c56
brandon-nava-huerta <brandon.nava.huerta@gmail.com>
2025-08-28 09:15:51 -0500

    project start

--- Indicadores.js
+++ Indicadores.js
@@ -1,6 +1,8 @@
   // Import the functions you need from the SDKs you need
-  import { initializeApp } from "firebase/app";
-  import { getDatabase, ref, set, onValue } from "firebase/app";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+
+  import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
